2d5a9ece36245c1f1f691ff412f20eca62eceecf
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-03-02 19:20:00 +0300

    task_01 added

---
+++ Task_01/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int InputInt(string msg)
+{
+    Console.Write($"{msg} -> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] Array(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(int item in array)
+    {
+        Console.WriteLine($"{item}");
+    }
+    Console.WriteLine();
+}
