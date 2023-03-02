274430a11dd05949fee5c0d0b2d903d144a358b4
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-03-02 19:38:37 +0300

    task_01 added

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -4,26 +4,45 @@
 
 int InputInt(string msg)
 {
-    Console.Write($"{msg} -> ");
-    return Convert.ToInt32(Console.ReadLine());
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
 }
 
-int[] Array(int size)
+int[] Array(int length)
 {
-    int[] array = new int[size];
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
+    int[] array = new int[length];
+        for (int i = 0; i < array.Length; i++)
     {
-        array[i] = rnd.Next(0, 100);
+        array[i] = InputInt($"Введите {i + 1}-й элемент");
     }
     return array;
 }
 
 void PrintArray(int[] array)
 {
-    foreach(int item in array)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.WriteLine($"{item}");
+        Console.WriteLine($"a[{i}] = {array[i]}");
     }
-    Console.WriteLine();
 }
+
+int SumPositive(int[]  array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+int length = InputInt("Введите количество чисел ->");
+int[] array;
+array = Array(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {SumPositive(array)}");
