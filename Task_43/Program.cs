5becda89308ad7320e0cb0bfde4eb549adc50949
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-03-05 17:29:35 +0300

    task_43 added

---
+++ Task_43/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int InputInt(string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
